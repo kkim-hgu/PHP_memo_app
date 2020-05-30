b8b02d9d7ba972e94988031ea660d4fe25d2965a
Kwang Kim <37907971+kkim-hgu@users.noreply.github.com>
2020-05-30 15:53:29 +0900

    Update memo.php

--- memo.php
+++ memo.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="ja">
+<html lang="ko">
 <head>
 <!-- Required meta tags -->
 <meta charset="utf-8">
@@ -17,14 +17,14 @@
 
 <main>
 <h2>Practice</h2>
-<!-- メモの詳細表示 -->
+<!-- 메모내용 표시 -->
 <?php
 require('dbconnect.php');
 
 $id = $_REQUEST['id'];
-// $idが数字であるか、正しい数値であるかの判定
+// $id가 숫자인지 체크
 if (!is_numeric($id) || $id <= 0) {
-    print('1以上の数字で指定してください');
+    print('1이상의 숫자이어야 합니다.');
     exit();
 }
 
@@ -35,11 +35,11 @@ $memo = $memos->fetch();
 <article>
     <pre><?php print($memo['memo']); ?></pre>
 
-    <a href="update.php?id=<?php print($memo['id']); ?>">編集する</a>
+    <a href="update.php?id=<?php print($memo['id']); ?>">수정</a>
     |
-    <a href="delete.php?id=<?php print($memo['id']); ?>">削除する</a>
+    <a href="delete.php?id=<?php print($memo['id']); ?>">삭제</a>
     |
-    <a href="index.php">戻る</a>
+    <a href="index.php">Return</a>
 </article>
 </main>
 </body>    
