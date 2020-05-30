8ec27184dcc56d0d8858d6b1526347fb62e855bc
Kwang Kim <37907971+kkim-hgu@users.noreply.github.com>
2020-05-30 15:38:18 +0900

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,9 +1,9 @@
 <?php
-// 処理の共通化
+// 공통처리구문
 try {
     $db = new PDO('mysql:dbname=mydb;host=localhost;charset=utf8',
     'root', 'root');
 } catch(PDOException $e) {
-    echo 'DB接続エラー：' . $e->getMessage();
+    echo 'DB Connection errot：' . $e->getMessage();
 }
 ?>
